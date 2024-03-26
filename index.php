5635d646265e3ad596387e2acf2fb713bc423db6
alisson10000 <alisson_lima_souza@yahoo.com.br>
2024-03-26 18:21:56 -0300

    primeiro commit
    versãoi 1

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="Author" content="Alisson lima"/>
+        <meta name="robots" content="index, follow"/>
+        <meta name="description" content="Aqui é o melhor portal para notícias." />
+        <meta name="keywords" content="esport, ciência, política"/>
+        <title>projeto</title>
+        <style>
+            @import "css/raiz.css";
+        </style>
+    </head>
+    <body>
+
+        <div class="containerGlobal">
+            <div class="top topo">
+                <p>fundec news</p>     <img src="img/logo.png" alt="alt"/>
+            </div>
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li><a href="url">home</a></li>
+                        <li><a href="url">esporte</a></li>
+                        <li><a href="url">política </a></li>
+                        <li><a href="url">ciência </a></li>
+                        <li><a href="url">arte </a></li>
+                        <li><a href="url">cinema </a></li>
+
+                    </ul>
+                </nav>
+
+            </div>
+            <div class="containerCentral"><!--<!-- Inicio -->
+
+
+                <div class="grid-container">
+
+                    <div class="area1">
+                        <section>
+                            <article class="class">
+                                <img src="img/brasilsub17.avif">
+                                <header><h1>Vitória no jogo da selecão feminia</h1></header>
+                            </article>
+                        </section>
+                        
+                    </div>
+                    <div class="area2"> <section>
+                            <article class="class">
+                                <img src="img/ciencia.jpg">
+                                <header><h1>Vitória no jogo da selecão feminia</h1></header>
+                            </article>
+                        </section></div>
+                    <div class="area3"> <section>
+                            <article class="class">
+                                <img src="img/kraven-o-cacador.jpg">
+                                <header><h1>Vitória no jogo da selecão feminia</h1></header>
+                            </article>
+                        </section></div>
+                    <div class="area4"> <section>
+                            <article class="class">
+                                <img src="img/politica.jpeg">
+                                <header><h1>Vitória no jogo da selecão feminia</h1></header>
+                            </article>
+                        </section></div>
+
+                </div>
+
+
+
+            </div><!--<!-- fim -->
+            <div class="footer">
+
+            </div>
+
+        </div>
+
+    </body>
+</html>
